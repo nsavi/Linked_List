6a72bd7d12afc25349dce20395ec079be407084e
nsavi <112178048+nsavi@users.noreply.github.com>
2023-04-20 09:21:40 +0530

    Create Bubble_sort.c

---
+++ Bubble_sort.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main()
+{
+    int n,i,j,a[5];
+    
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+    for(i = 0; i < n; i++)    
+    {    
+      for(j = 0; j < n; j++)    
+        {   
+            if(a[j]<a[j-1])
+            {
+                a[j] = a[j] ^ a[j-1];
+                a[j-1] = a[j] ^ a[j-1];
+                a[j] = a[j] ^ a[j-1];
+            }
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+    return 0;
+}
